15cc8a78a64fe42274132d36022076d8db82700a
Aunz <andy.lindinger@gmx.at>
2024-02-13 09:48:40 +0100

    SplitString Example

---
+++ Code Wars 1 Split Strings 6 kyu/Code Wars 1 Split Strings 6 kyu/Code Wars 1 Split Strings 6kyu/Program.cs
@@ -0,0 +1,10 @@
+﻿using Code_Wars_1_Split_Strings_6_kyu;
+
+
+string[] str = SplitString.Solution("faggott");
+
+
+foreach(string s in str)
+{
+    Console.WriteLine(s);
+}
---
+++ Code Wars 1 Split Strings 6 kyu/Code Wars 1 Split Strings 6 kyu/Code Wars 1 Split Strings 6kyu/SplitString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Wars_1_Split_Strings_6_kyu
+{
+    public static class SplitString
+    {
+
+        public static string[] Solution(string str)
+        {
+
+            List<string> list = new List<string>();
+            string saver = "";
+            int count = 0;
+            while(count < str.Length)
+            {
+                count += 2;
+                if (count > str.Length)
+                {
+                     saver = str.Substring(count-2)[0].ToString() + "_";
+                }
+                else
+                {
+                    saver = str.Substring(count-2)[0].ToString() + str.Substring(count - 1)[0].ToString();
+                }
+                list.Add(saver);
+            }
+
+
+
+            return list.ToArray(); 
+        }
+
+
+
+    }
+}
